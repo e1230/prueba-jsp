abd7845ab5d17562065b23c841841c1ca82c2047
Edgar Santos <embeltrans@correo.udistrital.edu.co>
2022-10-19 22:31:16 -1200

    Añadiendo credenciales Postgresql

--- src/main/java/config/Conexion.java
+++ src/main/java/config/Conexion.java
@@ -8,6 +8,7 @@ import java.awt.HeadlessException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.*;
 
 /**
  *
@@ -19,41 +20,42 @@ Group ID: org.postgresql
 Artifact ID: postgresql
 Versión: 9.4.1208
 
-*/
+ */
 public class Conexion {
+
     private Connection conectar;
     private String BD;
     private String usuario;
-    private String contrasenia;
+    private String contra;
 
-
-    public Conexion(Connection conectar, String BD, String usuario, String contrasenia) {
+    public Conexion(Connection conectar, String BD, String usuario, String contra) {
         this.conectar = conectar;
         this.BD = BD;
         this.usuario = usuario;
-        this.contrasenia = contrasenia;
+        this.contra = contra;
     }
 
-     public Conexion() {
+    public Conexion() {
         this.conectar = null;
-        this.BD = "";
-        this.usuario = "";
-        this.contrasenia = "";
+        this.BD = "\\\"jdbc:postgresql://ec2-54-211-77-\n"
+                + "238.compute-1.amazonaws.com:5432/(d463ke2mhm8np0)?sslmode=require\\";
+        this.usuario = "kmexxmxwgkmyii";
+        this.contra = "4432729b89254410e668fae7e926738dbddd3db0b590e86d05bfdbf34b89dffe";
     }
-    
-    public void conectar(){
-        try{
+
+    public void conectar() {
+        try {
             Class.forName("org.postgresql.Driver");
             this.BD = "";
             this.usuario = "";
-            this.contrasenia = "";
-            this.conectar = (Connection) DriverManager.getConnection(BD, usuario, contrasenia);
-        }catch(ClassNotFoundException | HeadlessException | SQLException e){
-            System.out.println("Error a conectar: "+e);
+            this.contra = "";
+            this.conectar = (Connection) DriverManager.getConnection(BD, usuario, contra);
+        } catch (ClassNotFoundException | HeadlessException | SQLException e) {
+            System.out.println("Error a conectar: " + e);
         }
     }
-    
-    public Connection getConexion(){
+
+    public Connection getConexion() {
         return conectar;
     }
 }
